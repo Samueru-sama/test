f99553f6775bd47773150cef6ee54f779c197266
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-02-17 15:53:36 -0400

    fuck

--- make-appimage.sh
+++ make-appimage.sh
@@ -18,7 +18,7 @@ export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/env
+quick-sharun /usr/bin/env /usr/bin/patchelf /usr/bin/zenity
 
 # Additional changes can be done in between here
 
